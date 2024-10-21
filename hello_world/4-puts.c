28b451635eaf14b5c63e7b17d762b81bc5bd696a
Rentaro009 <r3ntaro0@gmail.com>
2024-10-21 14:32:55 -0400

    Added a commit to it

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: this is test
+ *
+ * Return: Always 0 (Sucess)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
