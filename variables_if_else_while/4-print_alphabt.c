60f2b1dc84fbbb677664c75425bda25682993c46
Rentaro009 <r3ntaro0@gmail.com>
2024-10-24 09:48:45 -0400

    Made a C program tha prints the alphabet but the q and the e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet but the q and the e
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	if (ch != 'q' && ch != 'e')
+	{		
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
