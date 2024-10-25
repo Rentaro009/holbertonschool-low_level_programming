85eb322a27343c67523a3c69e6dd75f4edab0764
Rentaro009 <r3ntaro0@gmail.com>
2024-10-25 10:22:24 -0400

    Made a C program that prints the alphabet in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always void (Sucess)
+ */
+
+void print_alphabet(void)
+
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+
+}
