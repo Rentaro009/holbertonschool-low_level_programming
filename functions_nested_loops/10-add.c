a31bf7d97c6876bc98d0fa7611c2f7481b79b853
Rentaro009 <r3ntaro0@gmail.com>
2024-10-26 16:41:41 -0400

    Made C program that adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @num1: the value of int
+ * @num2: the value of int
+ *
+ * Return: Always 0
+ */
+
+int add(int num1, int num2)
+{
+	if (num1 != num2)
+	{
+		return (num1 + num2);
+	}
+	return (0);
+}
