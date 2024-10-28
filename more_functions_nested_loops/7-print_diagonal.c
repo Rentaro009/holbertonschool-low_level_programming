0d1e5b77bbfbf680df46685afe219e176ee1684b
Rentaro009 <r3ntaro0@gmail.com>
2024-10-28 14:53:32 -0400

    Made a C program that draws a diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ *
+ * @n: the number of times the character is printed
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int m, b;
+
+	if (n > 0)
+	{
+		for (m = 0; m < n; m++)
+		{
+			for (b = 0; b < m; b++)
+			_putchar(' ');
+			_putchar('\\');
+	
+			if (b == n - 1)
+			continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
