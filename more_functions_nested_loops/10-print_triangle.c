09c7bb8dd4c3910baabdaa0264eaed78fb001f6c
Rentaro009 <r3ntaro0@gmail.com>
2024-10-30 10:41:04 -0400

    Made a C program that prints a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int m, n;
+
+	if (size > 0)
+	{
+		for (m = 1; m <= size; m++)
+		{
+			for (n = size - m; n > 0; n--)
+				_putchar(' ');
+
+			for (n = 0; n < m; n++)
+				_putchar('#');
+		
+			if (m == size)
+				continue;
+				
+			_putchar('\n');
+		}
+	}
+		_putchar('\n');
+}
